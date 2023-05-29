8bc33439333679ae091250473965cf8d3f39a5de
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-05-29 17:48:02 +0530

    added a style in events listing page

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,11 +1,12 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 const EventList = ({ events }) => {
   return (
-    <div>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem key={event.id} event={event} />
       ))}
-    </div>
+    </ul>
   );
 };
 
