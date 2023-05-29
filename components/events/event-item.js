c69301c513a40ed058993334183fc0b96aaa0736
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-05-29 16:07:03 +0530

    added a arrow in explore event button

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,32 +1,49 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
+import { Fragment } from "react";
 
-const EventItem = ({ event }) => {
+function EventItem({ event }) {
   const { title, image, date, location, id } = event;
-  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
   const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
+
   return (
-    <Link href={exploreLink}>
-      <li className={classes.item}>
-        <img src={"/" + image} alt={title} />
-        <div className={classes.content}>
-          <div className={classes.summary}>
-            <h2>{title}</h2>
-            <div className={classes.date}>{formattedDate}</div>
-            <div className={classes.address}>{formattedAddress}</div>
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
           </div>
-          <div className={classes.actions}>
-            <Link href={exploreLink}>Explore Event</Link>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
           </div>
         </div>
-      </li>
-    </Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <Fragment>
+              <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+            </Fragment>
+          </Button>
+        </div>
+      </div>
+    </li>
   );
-};
+}
 
 export default EventItem;
