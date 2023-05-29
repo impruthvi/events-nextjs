f6405809d9eff556abd2efea37048f1da81bc90e
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-05-29 14:04:00 +0530

    implemented the event details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,27 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../dummy-data'
+import { Fragment } from 'react'
+import EventSummeary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+
 const EventDetailPage = () => {
+    const router = useRouter()
+    const { eventId } = router.query
+    const event = getEventById(eventId)
+
+    if(!event) {
+        return <p>No event found!</p>
+    }
+
     return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+        <Fragment>
+            <EventSummeary title={event.title} />
+            <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
 
