43098dc692d4e0666d74a32e604811a54dfad1f2
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-06-01 17:17:00 +0530

    replace the img tag with Image component

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -16,7 +16,7 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
